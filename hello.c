26541fdce2ece98742b5eef2a24a8bfc012fada5
Kathan Modh <kathanmodh25@gmail.com>
2024-08-09 14:02:36 +0530

    Printing 1 to 5
    added a function that prints 1 to 5 by using for loop

--- hello.c
+++ hello.c
@@ -1,7 +1,16 @@
-//Code written by kathan modh
+//Code Developed by kathan
 #include <stdio.h>
 
 int main()
 {
-    printf("hello world");
+    printf("hello world\n");
+
+// Function to print 1 to 5
+    int n = 5;
+    for (int i = 1; i < 6; i++)
+    {
+        printf("%d\n", i); 
+    }
+
+    return 0;
 }
